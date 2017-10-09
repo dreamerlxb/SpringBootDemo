9ad6fc5798bb28e1f40e70986427aefefe01d9da
dreamerlxb <1558485399@qq.com>
2017-10-09 16:34:03 +0800

    添加注释

--- src/test/java/com/lxb/demo/DemoApplicationTests.java
+++ src/test/java/com/lxb/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.lxb.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,37 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private com.lxb.demo.mail.MailService mailService;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testSimpleMail() throws Exception {
+		mailService.sendSimpleMail("424151385@qq.com", "test simple mail", " hello this is simple mail");
+	}
+
+	@Test
+	public void testHTMLMail() throws Exception {
+		String content = "<html>" +
+							"<body>" +
+								"<h3>hello world ! 这是一封Html邮件!</h3>" +
+								"<a href='https://baidu.com'>This is baidu</a>" +
+							"</body>" +
+						 "</html>";
+		mailService.sendHtmlMail("424151385@qq.com", "Test html mail", content);
+	}
+	
+	@Test
+	public void testAttachmentMail() {
+		String content = "<html>" +
+				"<body>" +
+					"<h3>Hello! This is a Attachment HTML mail!!</h3>" +
+					"<a href='https://baidu.com'>This is baidu</a>" +
+				"</body>" +
+			 "</html>";
+		mailService.sendAttachmentMail("424151385@qq.com", "Test Attachment mail", content);
+	}
 }
