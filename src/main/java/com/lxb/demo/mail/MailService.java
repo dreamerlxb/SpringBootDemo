a8a9233df359a73804e05669eb0fced8bf7a3982
dreamerlxb <1558485399@qq.com>
2017-10-09 16:33:20 +0800

    添加发送邮件

---
+++ src/main/java/com/lxb/demo/mail/MailService.java
@@ -0,0 +1,79 @@
+package com.lxb.demo.mail;
+
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailService {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@Value("${mail.from}")
+	private String from;
+
+	public void sendSimpleMail(String to, String subject, String content) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(from);
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(content);
+
+		try {
+			mailSender.send(message);
+			logger.info("简单邮件已经发送。");
+		} catch (Exception e) {
+			logger.error("发送简单邮件时发生异常！", e);
+		}
+	}
+	
+	
+	public void sendHtmlMail(String to, String subject, String content) {
+	    MimeMessage message = mailSender.createMimeMessage();
+	    try {
+	        //true表示需要创建一个multipart message
+	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	        helper.setFrom(from);
+	        helper.setTo(to);
+	        helper.setSubject(subject);
+	        helper.setText(content, true);
+//	        helper.
+
+	        mailSender.send(message);
+	        logger.info("html邮件发送成功");
+	    } catch (MessagingException e) {
+	        logger.error("发送html邮件时发生异常！", e);
+	    }
+	}
+	
+	public void sendAttachmentMail(String to, String subject, String content) {
+		MimeMessage message = mailSender.createMimeMessage();
+	    try {
+	        //true表示需要创建一个multipart message
+	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	        helper.setFrom(from);
+	        helper.setTo(to);
+	        helper.setSubject(subject);
+	        helper.setText(content, true);
+	        File file = new File("/Users/lion/Desktop/20160915110718912.png");
+	        helper.addAttachment("Test File", file);
+
+	        mailSender.send(message);
+	        logger.info("html邮件发送成功");
+	    } catch (MessagingException e) {
+	        logger.error("发送html邮件时发生异常！", e);
+	    }
+	}
+}
